fa53fdc51fb8ae1e1d8b9e4e2330003dcad900d6
AbramKanime <111146026+AbramKanime@users.noreply.github.com>
2023-10-23 12:38:04 +0100

    Made modifications to api.js

--- api.js
+++ api.js
@@ -1,3 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtxV7miqH6zz6FH5oPc9Bqil-N9X--EGM",
+  authDomain: "vanlife-5dfe9.firebaseapp.com",
+  projectId: "vanlife-5dfe9",
+  storageBucket: "vanlife-5dfe9.appspot.com",
+  messagingSenderId: "913300394662",
+  appId: "1:913300394662:web:9099f362b35aed467d9a67"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
